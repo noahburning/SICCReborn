3dc6fe7d6991402bf56c07d24ad4e589b158223f
Noah <noahnoe1@live.com>
2023-04-28 01:08:07 -0700

    Got login test to work again. Sets base for later.

---
+++ src/test/java/com/ana/api/repository/loginTest.java
@@ -0,0 +1,73 @@
+package com.ana.api.repository;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import java.sql.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes=com.ana.api.Application.class)
+public class loginTest {
+
+    @Test
+    @DisplayName("Test successful login")
+    public void testSuccessfulLogin() {
+        String dbUrl = "jdbc:mysql://siccbyana.ce2z68ofp2mx.us-east-1.rds.amazonaws.com:3306/cookieShopDB";
+        String username = "admin";
+        String password = "Dragonfly-Laurel-Serpent0-Purse-Scapegoat";
+        String enteredUsername = "jsmith";
+        String enteredPassword = "testpass123";
+
+        try (Connection conn = DriverManager.getConnection(dbUrl, username, password)) {
+            System.out.println("Connection established!");
+
+            // Prepare and execute the SQL statement
+            String sql = "SELECT * FROM User WHERE username=? AND password=?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, enteredUsername);
+            stmt.setString(2, enteredPassword);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.println("Login successful!");
+            } else {
+                fail("Login failed");
+            }
+
+        } catch (SQLException e) {
+            fail("SQL error occurred: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Test invalid login")
+    public void testInvalidLogin() {
+        String dbUrl = "jdbc:mysql://siccbyana.ce2z68ofp2mx.us-east-1.rds.amazonaws.com:3306/cookieShopDB";
+        String username = "admin";
+        String password = "Dragonfly-Laurel-Serpent0-Purse-Scapegoat";
+        String enteredUsername = "testuser";
+        String enteredPassword = "wrongpassword";
+
+        try (Connection conn = DriverManager.getConnection(dbUrl, username, password)) {
+            System.out.println("Connection established!");
+
+            // Prepare and execute the SQL statement
+            String sql = "SELECT * FROM User WHERE username=? AND password=?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, enteredUsername);
+            stmt.setString(2, enteredPassword);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                fail("Login succeeded unexpectedly");
+            } else {
+                System.out.println("Invalid username or password. Please try again.");
+            }
+
+        } catch (SQLException e) {
+            fail("SQL error occurred: " + e.getMessage());
+        }
+    }
+}
