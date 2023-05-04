5bd7a698cfa0cfb37c7449ad3b1af5fdfe03a94a
Ali Ahmed <78716016+a-amd@users.noreply.github.com>
2023-05-03 22:43:22 -0700

    refactor: use singleton pattern for ClientFrame.java

--- src/main/java/com/ana/Application.java
+++ src/main/java/com/ana/Application.java
@@ -13,7 +13,7 @@ import javax.swing.*;
 public class Application {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(ClientFrame::new);
+        SwingUtilities.invokeLater(ClientFrame::getInstance);
         SpringApplication.run(Application.class, args);
     }
 
