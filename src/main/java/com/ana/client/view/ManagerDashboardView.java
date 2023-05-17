889e273d121881578801937db5b345493184b632
Ali Ahmed <78716016+a-amd@users.noreply.github.com>
2023-05-17 16:41:30 -0700

    add: ManagerDashboardView.java

---
+++ src/main/java/com/ana/client/view/ManagerDashboardView.java
@@ -0,0 +1,10 @@
+package com.ana.client.view;
+
+import com.ana.client.listener.LogoutListener;
+import com.ana.client.listener.ReturnListener;
+
+public interface ManagerDashboardView {
+
+    void setLogoutListener(LogoutListener listener);
+
+}
